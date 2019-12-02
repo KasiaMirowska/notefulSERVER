92b99a8166ff45ca1fe13db416ff0fcdeeb1b289
Kasia <kasia@miroartdesign.com>
2019-12-02 11:26:19 -0500

    m

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
-    DB_URL: process.env.DATABASE_URL || 'postgresql://kamirska@localhost/noteful',
-    TEST_DB_URL: process.env.TEST_DATABASE_URL || 'postgresql://kamirska@localhost/noteful-test'
+    DATABASE_URL: process.env.DATABASE_URL || 'postgresql://kamirska@localhost/noteful',
+    TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://kamirska@localhost/noteful-test'
 }
